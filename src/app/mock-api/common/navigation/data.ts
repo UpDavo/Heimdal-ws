1c3254a60b2741660a35d2d88d20cfdd34464ac3
Anthony Villegas <advillegas@uees.edu.ec>
2021-09-20 11:26:57 -0500

    Customización inicial

---
+++ src/app/mock-api/common/navigation/data.ts
@@ -0,0 +1,214 @@
+/* eslint-disable */
+import { FuseNavigationItem } from '@fuse/components/navigation';
+
+export const defaultNavigation: FuseNavigationItem[] = [
+    {
+        id: 'analiticas.notificaciones',
+        title: 'Analíticas de uso',
+        subtitle: '',
+        type: 'group',
+        icon: 'heroicons_outline:home',
+        children: [
+            {
+                id: 'analiticas.resumen',
+                title: 'Resumen',
+                type: 'basic',
+                icon: 'heroicons_outline:clipboard-check',
+                link: '/analiticas/resumen',
+            },
+            {
+                id: 'analiticas.analiticas',
+                title: 'Analíticas',
+                type: 'basic',
+                icon: 'heroicons_outline:chart-pie',
+                link: '/analiticas/full',
+            },
+            {
+                id: 'analiticas.finanzas',
+                title: 'Finanzas',
+                type: 'basic',
+                icon: 'heroicons_outline:cash',
+                link: '/analiticas/finanzas',
+            },
+        ],
+    },
+    {
+        id: 'conf.notificaciones',
+        title: 'Notificaciones',
+        subtitle: '',
+        type: 'group',
+        icon: 'heroicons_outline:home',
+        children: [
+            {
+                id: 'apps.academy',
+                title: 'Academy',
+                type: 'basic',
+                icon: 'heroicons_outline:academic-cap',
+                link: '/apps/academy',
+            },
+            {
+                id: 'apps.contacts',
+                title: 'Contacts',
+                type: 'basic',
+                icon: 'heroicons_outline:user-group',
+                link: '/apps/contacts',
+            },
+            {
+                id: 'apps.notes',
+                title: 'Notes',
+                type: 'basic',
+                icon: 'heroicons_outline:pencil-alt',
+                link: '/apps/notes',
+            },
+            {
+                id: 'apps.scrumboard',
+                title: 'Scrumboard',
+                type: 'basic',
+                icon: 'heroicons_outline:view-boards',
+                link: '/apps/scrumboard',
+            },
+        ],
+    },
+    {
+        id: 'conf.general',
+        title: 'Configuración General',
+        subtitle: '',
+        type: 'group',
+        icon: 'heroicons_outline:document',
+        children: [
+            {
+                id: 'conf.factura',
+                title: 'Facturación',
+                type: 'basic',
+                icon: 'heroicons_outline:calculator',
+                link: '/configuraciones/pagos',
+            },
+            {
+                id: 'conf.perfil',
+                title: 'Configuración',
+                type: 'basic',
+                icon: 'heroicons_outline:cog',
+                link: '/configuraciones/perfil',
+            },
+        ],
+    },
+];
+export const compactNavigation: FuseNavigationItem[] = [
+    {
+        id: 'dashboards',
+        title: 'Dashboards',
+        tooltip: 'Dashboards',
+        type: 'aside',
+        icon: 'heroicons_outline:home',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id: 'apps',
+        title: 'Apps',
+        tooltip: 'Apps',
+        type: 'aside',
+        icon: 'heroicons_outline:qrcode',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id: 'pages',
+        title: 'Pages',
+        tooltip: 'Pages',
+        type: 'aside',
+        icon: 'heroicons_outline:document-duplicate',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id: 'user-interface',
+        title: 'UI',
+        tooltip: 'UI',
+        type: 'aside',
+        icon: 'heroicons_outline:collection',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id: 'navigation-features',
+        title: 'Navigation',
+        tooltip: 'Navigation',
+        type: 'aside',
+        icon: 'heroicons_outline:menu',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+];
+export const futuristicNavigation: FuseNavigationItem[] = [
+    {
+        id: 'dashboards',
+        title: 'DASHBOARDS',
+        type: 'group',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id: 'apps',
+        title: 'APPS',
+        type: 'group',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id: 'others',
+        title: 'OTHERS',
+        type: 'group',
+    },
+    {
+        id: 'pages',
+        title: 'Pages',
+        type: 'aside',
+        icon: 'heroicons_outline:document-duplicate',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id: 'user-interface',
+        title: 'User Interface',
+        type: 'aside',
+        icon: 'heroicons_outline:collection',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id: 'navigation-features',
+        title: 'Navigation Features',
+        type: 'aside',
+        icon: 'heroicons_outline:menu',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+];
+export const horizontalNavigation: FuseNavigationItem[] = [
+    {
+        id: 'dashboards',
+        title: 'Dashboards',
+        type: 'group',
+        icon: 'heroicons_outline:home',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id: 'apps',
+        title: 'Apps',
+        type: 'group',
+        icon: 'heroicons_outline:qrcode',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id: 'pages',
+        title: 'Pages',
+        type: 'group',
+        icon: 'heroicons_outline:document-duplicate',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id: 'user-interface',
+        title: 'UI',
+        type: 'group',
+        icon: 'heroicons_outline:collection',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id: 'navigation-features',
+        title: 'Misc',
+        type: 'group',
+        icon: 'heroicons_outline:menu',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+];
---
+++ src/app/modules/admin/dashboards/project/project.routing.ts
@@ -0,0 +1,13 @@
+import { Route } from '@angular/router';
+import { ProjectComponent } from 'app/modules/admin/analiticas/resumen/project.component';
+import { ProjectResolver } from 'app/modules/admin/analiticas/resumen/project.resolvers';
+
+export const projectRoutes: Route[] = [
+    {
+        path: '',
+        component: ProjectComponent,
+        resolve: {
+            data: ProjectResolver,
+        },
+    },
+];
